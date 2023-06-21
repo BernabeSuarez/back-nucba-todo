f85a1c8b76d11912ef7a086b1508b36f45c888dd
Bernabe <bernatattoo@gmail.com>
2023-06-21 01:23:33 -0300

    add update and delete controllers

--- controllers/todoControllers.js
+++ controllers/todoControllers.js
@@ -1,8 +1,40 @@
 import Todo from "../models/todo.js"
 
-export const createTodo = async (req, res) => {
+export const getTodo = async (req, res) => {
+    try {
+        const todos = await Todo.find()
+        res.status(200).json(todos)
 
-    const item = new Todo(req.body)
-    const saveItem = await item.save()
-    res.status(200).json(saveItem)
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+export const createTodo = async (req, res) => {
+    try {
+        const item = new Todo(req.body)
+        const saveItem = await item.save()
+        res.status(200).json(saveItem)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const updateTodo = async (req, res) => {
+    try {
+        const updateItem = await Todo.findByIdAndUpdate(req.params.id, { $set: req.body })
+        res.send("Item Update")
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const deleteTodo = async (req, res) => {
+    try {
+        const deleteItem = await Todo.findByIdAndDelete(req.params.id)
+        res.send("Deleted")
+    } catch (error) {
+
+    }
 }
--- routes/todo.js
+++ routes/todo.js
@@ -1,12 +1,11 @@
 import { Router } from 'express'
-import { createTodo } from '../controllers/todoControllers.js'
+import { createTodo, deleteTodo, getTodo, updateTodo } from '../controllers/todoControllers.js'
 
 const router = Router()
 
-router.get('/', (req, res) => {
-    res.send('Server running OK!')
-})
-
+router.get('/todos', getTodo)
 router.post('/todo', createTodo)
+router.put('/todo/:id', updateTodo)
+router.delete('/todo/:id', deleteTodo)
 
 export default router
